7be4e30ea52cf6654841de6bb699ead198fdd77a
ryosuke0206 <kyoho1988@gmail.com>
2020-02-28 22:45:22 +0900

    非同期通信
    完了

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,63 @@
+$(function(){
+    function buildHTML(message){
+      if ( message.image ) {
+        var html =
+        `<div class="message" data-message-id=${message.id}>
+            <div class="message-info">
+              <div class="message-info__name">
+                ${message.user_name}
+              </div>
+              <div class="message-info__data">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-info__text">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } 
+      else {
+        var html =
+        `<div class="message" data-message-id=${message.id}>
+        <div class="message-info">
+          <div class="message-info__name">
+            ${message.user_name}
+          </div>
+          <div class="message-info__data">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-info__text">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html); 
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+      $('form')[0].reset();
+      $('input').prop('disabled', false);
+    })
+  })
+});
