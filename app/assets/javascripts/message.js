d223dfc3c93be88b534cd174c29205161204dad4
ryosuke0206 <kyoho1988@gmail.com>
2020-03-08 17:12:55 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,6 +40,32 @@ $(function(){
       return html;
     };
   }
+  $('.new_message').on('submit', function(e) {
+    e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action'); 
+
+    $.ajax({
+      url: url, 
+      type: 'POST',  
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $(".form__submit").removeAttr("disabled");
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+
+    })
+    .fail(function(massege){
+      alert('メッセージを入力してください');
+      $(".form__submit").removeAttr("disabled");
+    })
+})
   var reloadMessages = function () {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       var last_message_id = $('.message:last').data("message-id"); 
